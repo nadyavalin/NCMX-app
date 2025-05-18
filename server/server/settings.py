82e2660dfbcbf220f7e24a9fc4bb3ebe54d3cbd3
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-05-18 16:16:13 +0300

    feat: install PostgreSQL on default

--- server/server/settings.py
+++ server/server/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ncmx_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
